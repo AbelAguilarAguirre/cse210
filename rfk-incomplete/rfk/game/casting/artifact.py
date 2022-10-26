9eaa7ed3abc3477ffe21760c010caeda93d95bd3
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-25 17:19:02 -1000

    121 to 210

---
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -0,0 +1,11 @@
+import re
+from game.casting.actor import Actor
+
+class Artifact(Actor):
+    def __init__(self):
+        self._message = ""
+
+    def get_message(self):
+        return self._message
+    def set_message(self,message):
+        self._message = message
